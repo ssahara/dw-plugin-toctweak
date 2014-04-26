a1923ed3bd45a1a40e8917b4c8e4a4f525e06db1
Satoshi Sahara <sahara.satoshi@gmail.com>
2014-04-26 21:33:26 +0900

    added option to disable built-in AutoTOC

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -8,4 +8,5 @@ $lang['tocPosition'] = 'Position of TOC box';
   $lang['tocPosition_o_0'] = 'default (top)';
   $lang['tocPosition_o_1'] = 'after First heading';
   $lang['tocPosition_o_2'] = 'after First Level 1 heading';
+  $lang['tocPosition_o_3'] = 'disable Auto-TOC';
 
