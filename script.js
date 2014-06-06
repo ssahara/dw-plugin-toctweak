67d5fdeb50406939361a1bc26f8cdfbb913b4a1f
Satoshi Sahara <sahara.satoshi@gmail.com>
2014-06-06 22:33:50 +0900

    fix JSINFO.toc undefined error

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
  * DokuWiki plugin TOC Tweak;
  */
 jQuery(function() {
-    // JSINFO.toc is to be defined in the Action toctweak closetoc component
-    if (typeof(JSINFO.toc.initial_state) != 'undefined') {
+    if (typeof(JSINFO.toc) != 'undefined') {
         var $toc = jQuery('#dw__toc h3');
         if ($toc.length) {
             $toc[0].setState(JSINFO.toc.initial_state);
