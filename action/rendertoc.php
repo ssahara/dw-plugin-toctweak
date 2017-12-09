4952382fee203e82eb9c6141f0b29221a86a4fd6
Satoshi Sahara <sahara.satoshi@gmail.com>
2017-12-10 01:05:57 +0900

    proof of concept step2

--- action/rendertoc.php
+++ action/rendertoc.php
@@ -17,133 +17,180 @@ class action_plugin_toctweak_rendertoc extends DokuWiki_Action_Plugin {
     /**
      * Register event handlers
      */
-    public function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, '_setTocControl');
-        $controller->register_hook('RENDERER_CONTENT_POSTPROCESS', 'BEFORE', $this, 'handlePostProcess', array());
-        $controller->register_hook('TPL_ACT_RENDER', 'BEFORE', $this, 'handle_act_render', array());
+    function register(Doku_Event_Handler $controller) {
+        $controller->register_hook('PARSER_CACHE_USE', 'BEFORE', $this, 'handleParserCache');
+        $controller->register_hook('TPL_ACT_RENDER', 'BEFORE', $this, 'handleActRender');
+        $controller->register_hook('RENDERER_CONTENT_POSTPROCESS', 'BEFORE', $this, 'handlePostProcess');
+    }
+
+    protected function _setupTocConfig() {
+        global $conf;
+
+        // Overwrite toc config parameters to catch up all headings in pages
+        //  toptoclevel : highest heading level which can appear in table of contents
+        //  maxtoclevel : lowest heading level to include in table of contents
+        //  tocminheads : minimum amount of headlines to show table of contents
+        // Note: setting tocminheads to 1 may cause trouble in preview.txt ?
+
+        $conf['toptoclevel'] = 1;
+        $conf['maxtoclevel'] = 5;
+        $conf['tocminheads'] = $this->getConf('tocminheads'); 
     }
 
     /**
-     * Overwrites TOC-related elements of $conf array
+     * PARSER_CACHE_USE
+     * Overwrite TOC config parameters to catch up all headings in pages
      */
-    public function _setTocControl(Doku_Event $event) {
-        global $conf, $INFO;
+    function handleParserCache(Doku_Event $event, $param) {  // _setTocControl
+        $this->_setupTocConfig();
+    }
 
-        if (isset($INFO['meta']['toc']['toptoclevel'])) {
-            $conf['toptoclevel'] = $INFO['meta']['toc']['toptoclevel'];
-        }
-        if (isset($INFO['meta']['toc']['maxtoclevel'])) {
-            $conf['maxtoclevel'] = $INFO['meta']['toc']['maxtoclevel'];
-        }
-        if (isset($INFO['meta']['toc']['toptoclevel'])||isset($INFO['meta']['toc']['maxtoclevel'])) {
-            $conf['tocminheads'] = 1;
+    /**
+     * TPL_ACT_RENDER
+     * Make sure the built-in TOC is not printed
+     */
+    function handleActRender(Doku_Event $event, $param) {
+        global $INFO, $ACT;
+        // TOC control should be changeable in only normal page
+        if (in_array($ACT, array('show', 'preview')) == false) return;
+        if (($INFO['meta']['toc']['position'] < 0)||($this->getConf('tocPosition') > 0)) {
+                $INFO['prependTOC'] = false;
+             // $INFO['prependTOC'] = true;  // DEBUG anyway show original TOC
         }
     }
- 
+
     /**
+     * RENDERER_CONTENT_POSTPROCESS
      * render TOC according to $tocPosition
      * -1: PLACEHOLDER set by syntax component
      *  0: default. TOC will not moved (tocPostion config option)
      *  1: set PLACEHOLDER after the first level 1 heading (tocPosition config optipn)
      *  6: set PLACEHOLDER after the first heading (tocPosition config option)
      */
-    public function handlePostProcess(Doku_Event $event, $param) {
-        global $INFO, $ID, $TOC, $ACT;
-        // TOC control should be changeable in only normal page
+    function handlePostProcess(Doku_Event $event, $param) {
+        global $INFO, $ID, $ACT, $TOC;
+
         if (in_array($ACT, array('show', 'preview')) == false) return;
+
+        $meta =& $INFO['meta']['toc'];
 
         // exclude sidebar, etc.
         if ($INFO['id'] != $ID) return;
 
-        // exclude <dokuwiki>/inc/lang/<ISO>/preview.txt file.
-        if ($ACT=='preview') {
-            if (preg_match('#(<h[1-6]).*?>(.*?)</\1>#', $event->data[1], $matches)) {
-                if ($matches[2] != hsc($INFO['meta']['title'])) return;
-            }
-        } 
-
         // TOC Position
-        $tocPosition = $this->getConf('tocPosition');
+        if (isset($meta['position'])) {
+            $tocPosition = $meta['position'];
+        } else {
+            $tocPosition = $this->getConf('tocPosition');
+        }
         if ($ACT=='preview') {
-            if ( (strpos($event->data[1], '<!-- TOC -->')) ||
-                 (strpos($event->data[1], '<!-- INLINETOC -->')) ) {
+            if (preg_match('#<!-- (?:TOC|INLINETOC) .*?-->#', $event->data[1])) {
                 $tocPosition = -1;
             }
-        } elseif (isset($INFO['meta']['toc']['position'])) {
-                $tocPosition = $INFO['meta']['toc']['position'];
         }
 
-        // set PLACEHOLDER according the tocPostion config setting
-        switch ($tocPosition) {
-            case 0:
-                //$event->data[1] = '<!-- TOC -->'.$event->data[1];
-                break;
-            case 1:
-                $event->data[1] = preg_replace('#</h1>#', "</h1>\n".'<!-- TOC -->', $event->data[1], 1);
-                break;
-            case 6:
-                $event->data[1] = preg_replace('#</(h[1-6])>#', "</$1>\n".'<!-- TOC -->', $event->data[1], 1);
-                break;
-        }
-
-        // replace PLACEHOLDER1s with tpl_toc() HTML output
-        if (strpos($event->data[1], '<!-- TOC -->') !== false) {
-            $html = tpl_toc(true);
-            $event->data[1] = str_replace('<!-- TOC -->', $html, $event->data[1]);
-            // add class to TOC box
-            if (isset($INFO['meta']['toc']['class'])) {
-                $search =  '<div id="dw__toc"';
-                $replace = $search.' class="'.$INFO['meta']['toc']['class'].'"';
-                $event->data[1] = str_replace($search, $replace, $event->data[1]);
+        // set PLACEHOLDER according to tocPostion config setting
+        if ($tocPosition >= 0) {
+            $topLv = (isset($meta['toptoclevel'])) ? $meta['toptoclevel'] : $this->getConf('toptoclevel');
+            $maxLv = (isset($meta['maxtoclevel'])) ? $meta['maxtoclevel'] : $this->getConf('maxtoclevel');
+            $tocClass = (isset($meta['class'])) ? $meta['class'] : '';
+            $placeHolder = '<!-- TOC '.$topLv.'-'.$maxLv.' '.$tocClass.' -->';
+            switch ($tocPosition) {
+                case 0:
+                    $event->data[1] = $placeHolder.DOKU_LF.$event->data[1];
+                    break;
+                case 1:
+                    $event->data[1] = preg_replace('#</h1>#', "</h1>\n".$placeHolder, $event->data[1], 1);
+                    break;
+                case 6:
+                    $event->data[1] = preg_replace('#</(h[1-6])>#', "</$1>\n".$placeHolder, $event->data[1], 1);
+                    break;
             }
         }
 
-        // replace PLACEHOLDER2s with tpl_inlinetoc() HTML output
-        if (strpos($event->data[1], '<!-- INLINETOC -->') !== false) {
-            $html = $this->tpl_inlinetoc(true);
-            $event->data[1] = str_replace('<!-- INLINETOC -->', $html, $event->data[1]);
-            // add class to TOC box
-            if (isset($INFO['meta']['toc']['class'])) {
-                $search =  '<div id="dw__inlinetoc"';
-                $replace = $search.' class="'.$INFO['meta']['toc']['class'].'"';
-                $event->data[1] = str_replace($search, $replace, $event->data[1]);
+        // replace PLACEHOLDERs
+        $placeHolder = '#<!-- (TOC|INLINETOC) (\d+)-(\d+)(?: (.*?))? -->#'; // regex
+
+        if (preg_match_all($placeHolder, $event->data[1], $tokens, PREG_SET_ORDER)) {
+
+            foreach ($tokens as $token) {
+
+                switch ($token[1]) {
+                    case 'TOC':
+                        $html = $this->html_toc($token[2], $token[3]);
+                        if (!empty($token[4])) {
+                            $search =  '<div id="dw__toc"';
+                            $replace = $search.' class="'.trim($token[4]).'"';
+                            $html = str_replace($search, $replace, $html);
+                        }
+                        break;
+                    case 'INLINETOC':
+                        $html = $this->html_inlinetoc($token[2], $token[3]);
+                        if (!empty($token[4])) {
+                            $search =  '<div id="dw__inlinetoc"';
+                            $replace = $search.' class="'.trim($token[4]).'"';
+                            $html = str_replace($search, $replace, $html);
+                        }
+                        break;
+                }
+                $event->data[1] = str_replace($token[0], $html, $event->data[1]);
             }
         }
     }
 
     /**
-     * Make sure the other TOC is not printed
+     * Return html of customized TOC
      */
-    public function handle_act_render(Doku_Event $event, $param) {
-        global $INFO, $ACT;
-        // TOC control should be changeable in only normal page
-        if (in_array($ACT, array('show', 'preview')) == false) return;
-        if (($INFO['meta']['toc']['position'] < 0)||($this->getConf('tocPosition') > 0)) {
-                $INFO['prependTOC'] = false;
+    private function html_toc($topLv, $maxLv) {
+        global $TOC;
+        if (!count($TOC)) return '';
+
+        $items = $this->trim_toc($TOC, $topLv, $maxLv);
+        $html = html_TOC($items); // use function in inc/html.php
+        return $html;
+    }
+
+    private function trim_toc(array $toc, $topLv, $maxLv) {
+        global $conf;
+
+        $items = array();
+        foreach ($toc as $item) {
+            // get header level from original toc level
+            $headerLv = $item['level'] + $conf['toptoclevel'] -1;
+            // get new toc level from header level
+            $tocLv = $headerLv - $topLv +1;
+
+            if (($headerLv < $topLv) || ($headerLv > $maxLv)) {
+                continue;
+            }
+            $item['level'] = $tocLv;
+            $items[] = $item;
         }
+        return $items;
     }
 
     /**
-     * Places the Inline TOC where the function is called
+     * Return html of customized INLINETOC
      */
-    function tpl_inlinetoc($return = false) {
-        global $lang, $TOC;
-        if (is_array($TOC)) {
+    private function html_inlinetoc($topLv, $maxLv) {
+        global $TOC;
+        if (!count($TOC)) return '';
+
+        $items = $this->trim_toc($TOC, $topLv, $maxLv);
+        if (!empty($items)) {
             $html = '<!-- INLINETOC START -->'.DOKU_LF;
             $html.= '<div id="dw__inlinetoc">'.DOKU_LF;
             $html.= '<h3>'.$lang['toc'].'</h3>';
-            $html.= html_buildlist($TOC, 'inlinetoc', array($this, 'html_list_inlinetoc'));
+            $html.= html_buildlist($items, 'inlinetoc', array($this, 'html_list_inlinetoc'));
             $html.= '</div>'.DOKU_LF;
             $html.= '<!-- INLINETOC END -->'.DOKU_LF;
         }
-        if ($return) return $html;
-        echo $html;
-        return '';
+        return $html;
     }
 
     /**
-     * Callback for html_buildlist.
-     * Builds list items with inlinetoc printable class 
+     * Callback for html_buildlist called from $this->html_inlinetoc()
+     * Builds list items with inlinetoc printable class
      */
     function html_list_inlinetoc($item) {
         if (isset($item['hid'])) {
